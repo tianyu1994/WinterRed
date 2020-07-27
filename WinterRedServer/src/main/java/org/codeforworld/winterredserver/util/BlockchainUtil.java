9a95224035b88de6f303a87e7d3d082df04d13db
ChenWenqi <tianyuyu_tianyu1994@163.com>
2020-07-27 15:27:26 +0800

    增加BlockChainUtil 类

---
+++ WinterRedServer/src/main/java/org/codeforworld/winterredserver/util/BlockchainUtil.java
@@ -0,0 +1,30 @@
+package org.codeforworld.winterredserver.util;
+
+import com.alibaba.fastjson.JSON;
+import org.codeforworld.winterredserver.entity.CheckMan;
+import org.codeforworld.winterredserver.entity.RumorInfo;
+
+import java.util.List;
+
+public class BlockchainUtil {
+    public static List<CheckMan> gainCheckManInfo(){
+//        JSON.
+
+        return null;
+    }
+
+    public static RumorInfo gainRumorInfo(Integer infoId){
+
+        return null;
+    }
+
+    public static List<RumorInfo> gainBatchRumorInfo(){
+
+        return null;
+    }
+
+    public static void pushRumorInfo2Chain(RumorInfo rumorInfo){
+
+    }
+
+}
