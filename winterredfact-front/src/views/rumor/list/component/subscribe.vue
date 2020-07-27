8a21f098e14a8c19f11038934b947d7b496927e0
Big_Cat <2499901339@qq.com>
2020-07-27 18:57:49 +0800

    新增获取邮箱验证码

--- winterredfact-front/src/views/rumor/list/component/subscribe.vue
+++ winterredfact-front/src/views/rumor/list/component/subscribe.vue
@@ -6,7 +6,12 @@
       <div style='padding:20px;'>
         <el-form :model='subscribeForm' :rules='subscribeRules' ref='subscribeForm'>
           <el-form-item label='邮箱' :label-width='formLabelWidth' prop='email'>
-            <el-input v-model='subscribeForm.email' autocomplete='off'></el-input>
+            <el-input v-model='subscribeForm.email' autocomplete='off'>
+              <el-button slot="append" :disabled="disabled" @click="sendCode">{{btntxt}}</el-button>
+            </el-input>
+          </el-form-item>
+          <el-form-item label='验证码' :label-width='formLabelWidth' prop='code'>
+            <el-input v-model='subscribeForm.code' autocomplete='off'></el-input>
           </el-form-item>
           <el-form-item label='订阅领域' :label-width='formLabelWidth' prop='field'>
             <el-checkbox-group v-model="subscribeForm.field">
@@ -27,9 +32,13 @@
 export default {
   data() {
     return {
+      disabled: false,
+      time: 0,
+      btntxt: '获取验证码',
       subscribeDialog: false,
       subscribeForm: {
         email: '',
+        code: '',
         field: []
       },
       formLabelWidth: '80px',
@@ -51,6 +60,9 @@ export default {
         email: [
           { required: true, message: '请输入邮箱', trigger: 'change' }
         ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'change' }
+        ],
         field: [
           { type: 'array', required: true, message: '请至少选择一个领域', trigger: 'change' }
         ]
@@ -64,9 +76,34 @@ export default {
     handleSubscribe() {
       this.$refs.subscribeForm.validate((valid) => {
         if (valid) {
-          alert('submit!')
+          console.log(this.subscribeForm)
         }
       })
+    },
+    sendCode() {
+      var regEmail = /^[A-Za-z1-9]+([-_.][A-Za-z1-9]+)*@([A-Za-z1-9]+[-.])+[A-Za-z]{2,5}$/
+      if (this.subscribeForm.email === '') {
+        this.$message.error('请先输入邮箱')
+      } else if (!regEmail.test(this.subscribeForm.email)) {
+        this.$message.error('请输入正确的邮箱')
+      } else {
+        console.log('发送验证吗')
+
+        this.time = 60
+        this.disabled = true
+        this.timer()
+      }
+    },
+    timer() {
+      if (this.time > 0) {
+        this.time--
+        this.btntxt = this.time + 's后重新获取'
+        setTimeout(this.timer, 1000)
+      } else {
+        this.time = 0
+        this.disabled = false
+        this.btntxt = '获取验证码'
+      }
     }
   }
 }
