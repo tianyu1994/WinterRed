f3b6f599305be4c56cbdf4c0fa98f73b0c760784
ganhaiying <1600014814@qq.com>
2020-07-28 11:12:15 +0800

    删除调试代码

--- WinterRedServer/src/main/java/org/codeforworld/winterredserver/WinterRedServerStartUp.java
+++ WinterRedServer/src/main/java/org/codeforworld/winterredserver/WinterRedServerStartUp.java
@@ -20,6 +20,5 @@ public class WinterRedServerStartUp {
         // 程序启动入口
         // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
         SpringApplication.run(WinterRedServerStartUp.class,args);
-        BlockchainUtil.gainBatchRumorInfo();
     }
 }
